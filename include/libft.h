805e56cc22a6761cf2c89ee96dc3c435270ab380
FxmousKid <iyan005@outlook.com>
2024-03-20 23:12:58 +0100

    Modifying adequatly libft.h

--- include/libft.h
+++ include/libft.h
@@ -1,20 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easy_standard_funcs.h                              :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 01:16:40 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/19 23:03:24 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/20 22:18:19 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASY_STANDARD_FUNCS_H
 # define EASY_STANDARD_FUNCS_H
 
-int ft_strlen(char *str);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_strlen(char *str);
+int		ft_atoi(char *str);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isalpha(int c);       
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_isalnum(int c);
 
-#endif
+#endif 
