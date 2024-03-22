fd9f4ca5f631245fb85a98b9c29ef188357123ef
FxmousKid <iyan005@outlook.com>
2024-03-22 22:49:18 +0100

    Fixed strlen to match the terminology of the man

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,21 +6,21 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 00:41:11 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 02:15:36 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/22 17:30:19 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+size_t	ft_strlen(char const *s)
 {
 	int	counter;
 
 	counter = 0;
-	if (str == NULL)
+	if (s == NULL)
 		return (0);
-	while (str[counter])
+	while (s[counter])
 		counter++;
 	return (counter);
 }
