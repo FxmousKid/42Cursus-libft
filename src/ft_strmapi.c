e1cb39ef7402914f941426a664b31d4ffa2608d8
FxmousKid <iyan005@outlook.com>
2024-03-23 00:10:12 +0100

    Finished + Tested ft_strmapi

--- src/ft_strmapi.c
+++ src/ft_strmapi.c
@@ -6,7 +6,28 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:54:48 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 01:54:49 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/23 00:09:51 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/libft.h"
+
+void	map_on_str(char *dest, char const *s, char (*f)(unsigned int, char))
+{
+	int	counter;
+
+	counter = -1;
+	while (s[++counter])
+		dest[counter] = f(counter, s[counter]);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*tab;
+
+	tab = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (tab == NULL)
+		return (NULL);
+	map_on_str(tab, s, f);
+	return (tab);
+}
