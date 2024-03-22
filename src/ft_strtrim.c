02887585b8b1eaddde3855737ce11e387ba2d39b
FxmousKid <iyan005@outlook.com>
2024-03-23 00:07:22 +0100

    normed ft_strtrim

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:51:45 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/22 22:42:20 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/22 22:59:17 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,12 @@ int	occ(char c, char const *s2)
 	}
 	return (0);
 }
+
 int	amt_end(char const *s1, char const *set)
 {
 	int	amount_to_trim;
 	int	counter_s;
-	
+
 	amount_to_trim = 0;
 	counter_s = ft_strlen(s1) - 1;
 	while (occ(s1[counter_s--], set))
@@ -40,7 +41,7 @@ int	amt_begin(char const *s1, char const *set)
 {
 	int	amount_to_trim;
 	int	counter_s;
-	
+
 	amount_to_trim = 0;
 	counter_s = 0;
 	while (occ(s1[counter_s++], set))
@@ -55,8 +56,8 @@ void	add_trim_str(char *dest, char const *s1, char const *set)
 	char	*tmp;
 
 	start = (amt_begin(s1, set));
-	end   = (ft_strlen(s1) - amt_end(s1, set));
-	tmp   = dest;
+	end = (ft_strlen(s1) - amt_end(s1, set));
+	tmp = dest;
 	if (*s1 == '\0')
 	{
 		*tmp = '\0';
@@ -71,9 +72,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*s1_trim;
 	int		size_s1_trim;
 
-	if (s1 == NULL || set == NULL) 
+	if (s1 == NULL || set == NULL)
 		return (NULL);
-	size_s1_trim = ft_strlen(s1) - amt_begin(s1, set) - amt_end(s1, set); 
+	size_s1_trim = ft_strlen(s1) - amt_begin(s1, set) - amt_end(s1, set);
 	s1_trim = (char *) malloc(sizeof(char) * (size_s1_trim + 1));
 	if (s1_trim == NULL)
 		return (NULL);
