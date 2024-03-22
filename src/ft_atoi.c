5ad9678e8f5d6a6dd8e698facf3ec91f31255324
FxmousKid <iyan005@outlook.com>
2024-03-22 22:47:27 +0100

    Fixing variable naming to match the man version

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,31 +6,31 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:05:43 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/22 11:39:48 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/22 17:42:58 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 
-int	ft_atoi(char *str)
+int	ft_atoi(char const *nptr)
 {
 	int	rendue;
 	int	sign;
 
 	rendue = 0;
 	sign = 1;
-	while (*str >= '\t' && *str <= '\r')
-		str++;
-	if (*str == '-' || *str == '+')
+	while (*nptr >= '\t' && *nptr <= '\r')
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
 	{
-		if (*str == '-')
+		if (*nptr == '-')
 			sign = -sign;
-		str++;
+		nptr++;
 	}
-	while (*str >= '0' && *str <= '9')
+	while (*nptr >= '0' && *nptr <= '9')
 	{
-		rendue = (*str - '0') + 10 * rendue;
-		str++;
+		rendue = (*nptr - '0') + 10 * rendue;
+		nptr++;
 	}
 	return (rendue * sign);
 }
