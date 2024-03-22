1321d8bc9859d7b2d3aa543ca4e71d3dac098157
FxmousKid <iyan005@outlook.com>
2024-03-22 22:50:59 +0100

    Added some functions and moved the librarys to here directly

--- include/libft.h
+++ include/libft.h
@@ -6,15 +6,21 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 01:16:40 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/21 23:25:47 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/22 22:31:12 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASY_STANDARD_FUNCS_H
 # define EASY_STANDARD_FUNCS_H
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int		ft_strlen(char *str);
-int		ft_atoi(char *str);
+
+size_t		ft_strlen(char const *s);
+int		ft_atoi(char const *nptr);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_isalpha(int c);       
@@ -22,10 +28,12 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
-int		count_words(char *str, char *charset);
 
 char	*ft_itoa(int num);
-char	**ft_split(char *str, char *charset);
-char	*ft_strjoin(int size, char **strs, char *sep);
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s1, char const *set);
+
 
 #endif 
