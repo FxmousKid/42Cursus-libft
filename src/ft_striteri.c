d119edc3fd5c4c3874a0029de523942987829ac2
FxmousKid <iyan005@outlook.com>
2024-03-23 19:55:01 +0100

    Finished + Tested ft_striteri

--- src/ft_striteri.c
+++ src/ft_striteri.c
@@ -6,7 +6,18 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:56:21 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 01:56:22 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/23 19:46:54 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	counter;
+
+	counter = 0;
+	while (s[counter])
+	{
+		f(counter, s + counter);
+		counter++;
+	}
+}
