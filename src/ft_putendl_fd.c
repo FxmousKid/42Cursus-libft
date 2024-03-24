c45348328c256bbe05f34abb5576cc00954aeb25
FxmousKid <iyan005@outlook.com>
2024-03-24 01:57:38 +0100

    Finished + Tested ft_putendl_fd

--- src/ft_putendl_fd.c
+++ src/ft_putendl_fd.c
@@ -6,7 +6,14 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:59:10 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 01:59:10 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/23 23:32:56 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
