7e690a2ad9aed567a95ec2fa7a74dd095fd6e1db
FxmousKid <iyan005@outlook.com>
2024-03-24 01:57:20 +0100

    Finished + Tested ft_putchar_fd

--- src/ft_putchar_fd.c
+++ src/ft_putchar_fd.c
@@ -6,7 +6,13 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:59:31 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 01:59:32 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/24 01:56:08 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
