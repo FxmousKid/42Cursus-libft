31ed33da9c769928f79cf12ee0e20401754dd699
FxmousKid <iyan005@outlook.com>
2024-03-24 01:54:58 +0100

    Adding the functions done for the day

--- include/libft.h
+++ include/libft.h
@@ -6,28 +6,31 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 01:16:40 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/23 01:55:24 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/24 00:57:59 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EASY_STANDARD_FUNCS_H
-# define EASY_STANDARD_FUNCS_H
-#include <string.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <limits.h>
+#ifndef LIBFT_H
+# define LIBFT_H
 
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-size_t		ft_strlen(char const *s);
+size_t	ft_strlen(char const *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
 int		ft_atoi(char const *nptr);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_isalpha(int c);       
+int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_itoa(int num);
 char	**ft_split(char const *s, char c);
@@ -35,6 +38,13 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strnstr(const char *big, const char *small, size_t len);
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif 
