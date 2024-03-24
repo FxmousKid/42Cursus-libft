6d2a87334da67437fae8e6aa9746dccd37b5c1fe
FxmousKid <iyan005@outlook.com>
2024-03-24 01:58:20 +0100

    Finished + Tested ft_putstr_fd

--- src/ft_putstr_fd.c
+++ src/ft_putstr_fd.c
@@ -6,7 +6,17 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:58:06 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 01:58:07 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/23 22:10:00 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	counter;
+
+	counter = 0;
+	while (s[counter])
+		write(fd, &s[counter++], 1);
+}
