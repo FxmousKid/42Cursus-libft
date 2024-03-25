f11769c27708856ad9557ce14e224a21765dcf37
FxmousKid <iyan005@outlook.com>
2024-03-25 15:56:10 +0100

    Finished + Tested ft_lstdelone

--- src/bonus/ft_lstdelone_bonus.c
+++ src/bonus/ft_lstdelone_bonus.c
@@ -1,12 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:21:22 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 02:21:23 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/25 15:50:30 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst == NULL || del == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+	lst = NULL;
+}
