c389c364dc828f36e1ad91aef923608c09cbfb6d
FxmousKid <iyan005@outlook.com>
2024-03-25 01:45:41 +0100

    Finishing libft header for standard funcs

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 01:16:40 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/24 00:57:59 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/25 00:03:10 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <string.h>
 # include <fcntl.h>
 # include <unistd.h>
+# include <limits.h>
 # include <stdlib.h>
 
 size_t	ft_strlen(char const *s);
@@ -31,6 +32,7 @@ int		ft_isprint(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 char	*ft_itoa(int num);
 char	**ft_split(char const *s, char c);
@@ -39,12 +41,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strnstr(const char *big, const char *small, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrch(const char *s, int c);
+char	*ft_strdup(const char *s);
 
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void	*dest, const void *src, size_t n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif 
