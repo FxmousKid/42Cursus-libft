a775e44d842b8b23d500357608219f30d1fac77e
FxmousKid <iyan005@outlook.com>
2024-03-25 15:57:24 +0100

    Finished + Tested ft_lstadd_front

--- src/bonus/ft_lstadd_front_bonus.c
+++ src/bonus/ft_lstadd_front_bonus.c
@@ -1,12 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:23:10 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 02:23:11 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/25 11:18:58 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst == NULL)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
