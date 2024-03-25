9d8e5cb03613f6d677c23070691d34bba68de36d
FxmousKid <iyan005@outlook.com>
2024-03-25 15:56:52 +0100

    Finished + Tested ft_lstsize

--- src/bonus/ft_lstsize_bonus.c
+++ src/bonus/ft_lstsize_bonus.c
@@ -1,12 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:22:42 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 02:22:43 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/25 12:38:29 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	if (lst->next == NULL)
+		return (1);
+	return (ft_lstsize(lst->next) + 1);
+}
