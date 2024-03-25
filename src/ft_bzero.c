9652daabb45e414c41aca1d3d04d49f8fabd3397
FxmousKid <iyan005@outlook.com>
2024-03-25 01:46:07 +0100

    Finished + Tested ft_bzero

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -6,7 +6,20 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:05:31 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 02:05:31 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/24 23:00:14 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/libft.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	counter;
+
+	if (s == NULL)
+		return ;
+	counter = 0;
+	while (counter < n)
+		((char *)s)[counter++] = 0;
+	return ;
+}
