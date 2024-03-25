0e3fa695d51a41d0fd356ce4a605ed0d3ade011f
FxmousKid <iyan005@outlook.com>
2024-03-25 15:56:21 +0100

    Finished + Tested ft_lstnew

--- src/bonus/ft_lstnew_bonus.c
+++ src/bonus/ft_lstnew_bonus.c
@@ -1,12 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:23:54 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/20 02:23:55 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/25 11:43:54 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*lst;
+
+	lst = (t_list *) malloc(sizeof(t_list));
+	if (lst == NULL)
+		return (NULL);
+	lst->content = content;
+	lst->next = NULL;
+	return (lst);
+}
