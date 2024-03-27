32024ca7c279ae3d62b4b8c1b54cb8f3db28e7a9
FxmousKid <iyan005@outlook.com>
2024-03-27 12:12:05 +0100

    Adde ft_lstaddfront

---
+++ ft_lstadd_front_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 02:23:10 by inazaria          #+#    #+#             */
+/*   Updated: 2024/03/25 18:28:09 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst == NULL)
+		return ;
+	new->next = *lst;
+	lst = &new;
+}
