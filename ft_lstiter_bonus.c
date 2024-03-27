f9fd1361c2de947d62e1a38ad8bd83d77b8b4983
FxmousKid <iyan005@outlook.com>
2024-03-27 12:12:18 +0100

    Added listiter

---
+++ ft_lstiter_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 02:17:20 by inazaria          #+#    #+#             */
+/*   Updated: 2024/03/26 14:53:45 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst == NULL)
+		return ;
+	f(lst->content);
+	return (ft_lstiter(lst->next, f));
+}
