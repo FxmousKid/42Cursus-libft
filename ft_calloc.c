07abfbed215d02bc2b76906f9f17b812d036a0ae
FxmousKid <iyan005@outlook.com>
2024-03-27 12:32:49 +0100

    Modified the header path of calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 #include <stdio.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
