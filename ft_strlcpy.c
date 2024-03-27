b482f080634cc1fa8ab17a155ad9d6210aed246e
FxmousKid <iyan005@outlook.com>
2024-03-27 12:35:07 +0100

    Modified the header path of strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
