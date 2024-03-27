0b633979cc136bfbd1b881580629627834009e45
FxmousKid <iyan005@outlook.com>
2024-03-27 12:33:19 +0100

    Modified the header path of itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 static int	powerof10(int n)
 {
