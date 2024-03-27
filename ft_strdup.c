4ef2cfe8da3081404d54efbf5601291890621b2c
FxmousKid <iyan005@outlook.com>
2024-03-27 12:34:57 +0100

    Modified the header path of strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 #include <unistd.h>
 
 char	*ft_strdup(const char *s)
