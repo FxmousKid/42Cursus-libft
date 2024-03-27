6692a08bc8ce9febcc58deaca1b393dcdc0040ba
FxmousKid <iyan005@outlook.com>
2024-03-27 12:33:43 +0100

    Modified the header path of lstiter

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/libft.h"
+#include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
