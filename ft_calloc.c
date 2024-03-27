d81dd4db1b25af6bf5dda0d03af69084ea03c906
FxmousKid <iyan005@outlook.com>
2024-03-27 12:00:08 +0100

    Added calloc

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 02:05:07 by inazaria          #+#    #+#             */
+/*   Updated: 2024/03/25 01:44:05 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+#include <stdio.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*tab;
+
+	if (nmemb && size && nmemb > UINT_MAX / size)
+		return (NULL);
+	tab = malloc(nmemb * size);
+	if (tab == NULL)
+		return (NULL);
+	ft_bzero(tab, nmemb * size);
+	return (tab);
+}
