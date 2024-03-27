dd36b869f50a37699e380559d74e3ff3caea17c0
FxmousKid <iyan005@outlook.com>
2024-03-27 12:34:29 +0100

    Modified the header path of memset

--- ft_memset.c
+++ ft_memset.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
 {
