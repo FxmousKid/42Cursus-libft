5ad29303edaa63ed1ef89e8b7aba830be7d6f53d
FxmousKid <iyan005@outlook.com>
2024-03-27 12:32:34 +0100

    find . -nModified the header path of atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 int	ft_atoi(char const *nptr)
 {
