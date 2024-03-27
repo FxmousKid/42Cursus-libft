545214ffb427b640ad68fb3058f0cc1861e42807
FxmousKid <iyan005@outlook.com>
2024-03-27 12:34:20 +0100

    Modified the header path of memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
