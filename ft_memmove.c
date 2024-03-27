e27c1bf9eecd24536f80b5b4b6ac4e06be604621
FxmousKid <iyan005@outlook.com>
2024-03-27 12:34:27 +0100

    Modified the header path of memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
