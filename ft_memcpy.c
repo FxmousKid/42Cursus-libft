073997dffdb5d6a9eed93632f3f2c623dacdd591
FxmousKid <iyan005@outlook.com>
2024-03-27 12:34:23 +0100

    Modified the header path of memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
