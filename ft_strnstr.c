3e2a3b2d823314c6ba8b79a0ceba42297efd6d3c
FxmousKid <iyan005@outlook.com>
2024-03-27 12:35:16 +0100

    Modified the header path of strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *small, size_t len)
 {
