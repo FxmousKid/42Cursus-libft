fae7cc030736721a1f3a92fe96c722c77eb2e105
FxmousKid <iyan005@outlook.com>
2024-03-27 12:34:55 +0100

    Modified the header path of strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
