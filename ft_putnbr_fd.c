4578f1ccaa89b550e22202ebec3f593bf01d920c
FxmousKid <iyan005@outlook.com>
2024-04-01 13:58:00 +0200

    Corrected ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:58:23 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/23 22:20:29 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/01 12:56:11 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
+	{
 		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
 	else
 	{
 		if (n < 0)
 		{
 			ft_putchar_fd('-', fd);
-			n = n * -1;
+			n = -n;
 		}
-		if (n >= 9)
+		if (n > 9)
 		{
 			ft_putnbr_fd(n / 10, fd);
 		}
