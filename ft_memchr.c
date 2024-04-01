d51e22b5eaafc5c1687838f5b352fbd933116fe5
FxmousKid <iyan005@outlook.com>
2024-04-01 13:57:54 +0200

    Correct ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:01:53 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/24 17:32:05 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/01 13:55:44 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	counter;
+	unsigned char	*ptr;
+	unsigned char	cs;
+	size_t			i;
 
-	counter = 0;
-	while (counter < n)
+	ptr = (unsigned char *) s;
+	cs = (unsigned char) c;
+	i = 0;
+	while (i < n)
 	{
-		if (*(unsigned char *)(s + counter) == c)
-			return ((void *) s + counter);
-		counter++;
+		if (*(ptr + i) == cs)
+		{
+			return (ptr + i);
+		}
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
